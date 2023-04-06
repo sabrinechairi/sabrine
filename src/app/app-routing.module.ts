2987e1303e99307f6d0d5e09a7bea02d60f232d2
AyaeI <127328563+AyaeI@users.noreply.github.com>
2023-04-06 20:36:50 +0000

    upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilieresComponent } from './components/filieres/filieres.component';
+import { LoginComponent } from './components/login/login.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'filiere', component:FilieresComponent,
+    pathMatch: 'full'
+  },
+  {path:'login', component:LoginComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'home',component:HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FilieresComponent } from './components/filieres/filieres.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FilieresComponent,
+    ContactComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
